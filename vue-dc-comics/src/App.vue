4f112ae02f8229d34147dd5ad00e7cbe7163aae6
MatteoRossi01 <93929073+MatteoRossi01@users.noreply.github.com>
2022-02-21 22:55:46 +0100

    Aggiunta box-link

--- vue-dc-comics/src/App.vue
+++ vue-dc-comics/src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
       <myHeader></myHeader> 
       <myMain></myMain>
+      <myLinkBox></myLinkBox>
   </div>
 </template>
 
 <script>
 import myHeader from './components/myHeader.vue'
 import myMain from './components/myMain.vue'
+import myLinkBox from './components/myLinkBox.vue'
 
 export default {
   name: 'App',
   components: {
     myHeader,
     myMain,
+    myLinkBox,
   }
 }
 </script>
---
+++ vue-dc-comics/src/components/myLinkBox.vue
@@ -0,0 +1,97 @@
+<template>
+    <div class="container-link-box">
+
+        <div class="link-box">
+            <a href="#" class="link-card">
+                <div class="box-img">
+                    <img src="../assets/img/buy-comics-digital-comics.png" alt="link-img">
+                </div>
+                <div class="box-txt">
+                    <span>DIGITAL COMICS</span>
+                </div>
+            </a>  
+
+            <a href="#" class="link-card">
+                <div class="box-img">
+                    <img src="../assets/img/buy-comics-merchandise.png" alt="link-img">
+                </div>
+                <div class="box-txt">
+                    <span>DC MERCHANDISE</span>
+                </div>
+            </a>  
+
+            <a href="#" class="link-card">
+                <div class="box-img">
+                    <img src="../assets/img/buy-comics-subscriptions.png" alt="link-img">
+                </div>
+                <div class="box-txt">
+                    <span>SUBSCRIPTIONS</span>
+                </div>
+            </a>  
+
+            <a href="#" class="link-card">
+                <div class="box-img">
+                    <img src="../assets/img/buy-comics-shop-locator.png" alt="link-img">
+                </div>
+                <div class="box-txt">
+                    <span>COMIC SHOP LOCATOR</span>
+                </div>
+            </a>  
+
+            <a href="#" class="link-card">
+                <div class="box-img">
+                    <img src="../assets/img/buy-dc-power-visa.svg" alt="link-img">
+                </div>
+                <div class="box-txt">
+                    <span>DC POWER VISA</span>
+                </div>
+            </a>  
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'myLinkBox',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    @import "../assets/style/variables.scss";
+    .container-link-box {
+        background-color: $Blue;
+      
+        .link-box {
+            width: 70%;
+            margin: auto;
+            height: 120px;
+            @include my_row_flex;
+            justify-content: space-between;
+
+            .link-card {
+                @include my_row_flex;
+                justify-content: space-around;
+                text-transform: uppercase;
+                color: $White;
+                font-size: 13px;
+                text-decoration: none;
+
+                .box-img {
+                    width: 50px;
+                    height: 50px;
+                    margin-right: 5px;
+
+                    img {
+                        width: 100%;
+                        height: 100%;
+                        object-fit: contain;
+                    }
+                }
+            }
+        }
+    }
+</style>
